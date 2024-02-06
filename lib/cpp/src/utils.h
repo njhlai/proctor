6793509d82966c11008b553aac827c02b2d07b57
njhlai <nicholasjhlai@gmail.com>
2024-02-06 13:52:33 -0800

    lib: cpp: Removed unnecessary code

--- lib/cpp/src/utils.h
+++ lib/cpp/src/utils.h
@@ -7,16 +7,6 @@
 #include <vector>
 
 template <typename T>
-bool vectorEqualityWithoutOrdering(std::vector<T>& a, std::vector<T>& b) {
-    if (a.size() != b.size()) return false;
-
-    std::sort(a.begin(), a.end());
-    std::sort(b.begin(), b.end());
-
-    return a == b;
-}
-
-template <typename T>
 std::ostream& operator<<(std::ostream& os, const std::vector<T>& value) {
     os << "[";
     for (auto el : value) os << el << ",";
